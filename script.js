86b2340c6d35f23ee28c437bfbe99bf789db4098
Burrpie <135572400+Burrpiee@users.noreply.github.com>
2024-12-22 22:33:54 +0800

    Calculator is now able to perform basic functions
    Calculator can now operate on 2 numbers and clear
    the display.

--- script.js
+++ script.js
@@ -1,25 +1,78 @@
-function add(num1, num2){
-    return num1 + num2;
-}
-
-function subtract(num1, num2){
-    return num1 - num2;
-}
-
-function multiply(num1, num2){
-    return num1 * num2;
-}
-
-function divide(num1, num2){
-    return num1 / num2;
-}
-
 let firstNum;
 let operator;
 let secondNum;
+let result = 0;
+let step = 0;
 
-let display = document.querySelector(".display");
+let numArray = [];
+let secondNumArray = [];
+
+const display = document.querySelector(".display");
+
+function getNum(num) {
+    if (step === 0 || step === 1){
+        numArray.push(num);
+        step = 1;
+        firstNum = Number(numArray.join(''));
+        display.textContent = firstNum;
+    }
+    else if (step === 2){
+        secondNumArray.push(num)
+        secondNum = Number(secondNumArray.join(''));
+        display.textContent = secondNum;
+    }
+}
+
+function getOp(op) {
+    operator = op;
+    step = 2;
+}
 
 function operate() {
-
+    switch (operator){
+        case '+':
+            result = add(firstNum, secondNum);
+            display.textContent = result;
+            break;
+        case '-':
+            result = subtract(firstNum, secondNum);
+            display.textContent = result;
+            break;
+        case '*':
+            result = multiply(firstNum, secondNum);
+            display.textContent = result;
+            break;
+        case '/':
+            result = divide(firstNum, secondNum);
+            display.textContent = result;
+            break;
+    }
 }
+
+function clearDisplay() {
+    display.textContent = 0;
+    firstNum = null;
+    secondNum = null;
+    operator = null;
+    numArray = [];
+    secondNumArray = [];
+    result = 0;
+    step = 0;
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
